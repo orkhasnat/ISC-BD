03e106f58852df51880be9c874319a8f6999f809
orkhasnat <orkhasnat@gmail.com>
2024-02-16 23:03:46 +0600

    location added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   server: {
